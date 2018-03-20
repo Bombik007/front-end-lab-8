1ad3f11d9885119ca69c9f0899051c13f4463a25
Bohdan Babyak <bodiababiak@gmail.com>
2018-03-20 03:20:10 +0200

    first commit bl

---
+++ homework_02/js/script.js
@@ -0,0 +1,22 @@
+﻿$(window).load(function(){
+    var maxHeight = 0;
+    for( let i=0; i < $('.category-unit').length; i++) {
+        if ($('.category-unit').eq(i).height() > maxHeight) {
+            maxHeight = $('.category-unit').eq(i).height()
+        }
+    }
+    for( let i=0; i < $('.category-unit').length; i++) {
+        $('.category-unit').eq(i).css('height', maxHeight);
+    }
+
+    $('.flexslider').flexslider({
+        animation: "fade"
+      });
+
+    $(document).on('submit','.form',function(){
+    	if( $('.form #name').val().length < 6) {
+    		alert('Name must contain at least 6 characters');
+    		return false;
+    	}
+    });
+});
