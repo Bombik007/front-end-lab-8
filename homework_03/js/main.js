3f6c76169734b4c3e7821867f51f6052176847da
Bohdan Babyak <bodiababiak@gmail.com>
2018-03-24 23:07:42 +0200

    company constructor 90% done

--- homework_03/js/main.js
+++ homework_03/js/main.js
@@ -3,36 +3,63 @@ function Company(name, owner, maxCount) {
     this._owner = owner;
     this._maxCount = maxCount;
     this.timeInCompany;
+    this._employeeList = [];
+    this._logs = [];
 
-    let _employeeList = [];
-    let _logs = [];
     
-    this.addNewEmployee = function() {
-
+    this.addNewEmployee = function(emp) {
+        _employeeList.push(emp);
+        _logs.push("addNewEmployee");
     }
 
     this.removeEmployee = function() {
+        let resultSalary = 0;
+        let index;
 
+        for (let i = 0; i < _employeeList.length; i++) {
+            if (_employeeList[i].salary > resultSalary) {
+                resultSalary = _employeeList[i].salary;
+                index = i;
+            }
+        }
+        _employeeList.splice(i, 1);
+        _logs.push("removeEmployee");
     }
-
     this.getAvarageSalary = function() {
+        let sum = 0;
 
+        _employeeList.forEach(function(item) {
+            sum += _employeeList[item].salary;
+        });
+
+        _logs.push("getAvarageSalary");
+        return sum/_employeeList.length;
     }
 
     this.getEmployees = function() {
-
+        _logs.push("getEmployees");
+        return _employeeList;
     }
 
     this.getFormattedListOfEmployees = function() {
+        _logs.push("getFormattedListOfEmployees");
         console.log(`${name} -  works in ${companyName} ${timeInCompany} seconds`);
     }
 
     this.getAvarageAge = function() {
+        let averageAge = 0;
 
+        _employeeList.forEach(function(item) {
+            averageAge += _employeeList[item].salary;
+        });
+
+        _logs.push("getAvarageAge");
+        return averageAge/_employeeList.length;
     }
 
     this.getHistory = function() {
-
+        _logs.push("getHistory");
+        return _logs;
     }
 }
 
@@ -42,7 +69,7 @@ function Employee(name, primarySkill, age, salary) {
     this._age = age;
     this._salary = salary;
     this._workTime;
-    this._log;
+    this.employeeLog;
 
     this.getSalary = function() {
         return this._salary;
@@ -56,8 +83,8 @@ function Employee(name, primarySkill, age, salary) {
 
     }
 
-    this.hire = function() {
-
+    this.hire = function(company) {
+        
     }
 
     this.fire = function() {
@@ -65,6 +92,6 @@ function Employee(name, primarySkill, age, salary) {
     }
 
     this.getHistory = function() {
-
+        return employeeLog;
     }
 }
