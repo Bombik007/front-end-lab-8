12446e21117644d610276832c7318ed0e49e4b19
Bohdan Babyak <bodiababiak@gmail.com>
2018-04-03 03:23:46 +0300

    15% done

---
+++ homework_06/js/task2.js
@@ -0,0 +1,48 @@
+let validationServer = "https://shrouded-garden-94580.herokuapp.com/";
+let outerContainer = document.getElementById("response");
+
+
+let template = {
+    'IP Address': obj.ip,
+    'City': obj.city,
+    'Region': obj.region,
+    'Country': `${obj.country}/${obj.country_name}`,
+    'Postal Code': obj.postal,
+    'Latitude / Longitude': `${obj.latitude}, ${obj.longitude}`,
+    'Time Zone': `${obj.timezone}(${obj.utc_offset})`,
+    'Currency': obj.currency,
+    'Languages': obj.languages,
+    'ASN': obj.asn,
+    'Org': obj.org
+};
+
+let generateInfo = obj => {
+    let table = document.createElement("table");
+
+    for (let i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            let tr = document.createElement("tr"),
+                nameCell = document.createElement("td"),
+                valueCell = document.createElement("td");
+            
+            nameCell.appendChild(i);
+            valueCell.appendChild(obj[i]);
+            tr.appendChild(nameCell);
+            tr.appendChild(valueCell);
+        }
+    }
+    outerContainer.appendChild(table);
+}
+
+let createMap = (lat, lng) => {
+    let coordinates = new google.maps.LatLng(lat, lng);
+    let opt = {
+        center: centerLatLng,
+        zoom: 6
+    };
+    let mapBody = new google.maps.Map(document.getElementById("map"), opt);
+    let marker = new google.maps.Marker({
+        position: coordinates,
+        map: mapBody
+    });
+}
