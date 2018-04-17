59c03342bc792343682a59ac9867d0b4900bc31d
Bohdan Babyak <bodiababiak@gmail.com>
2018-04-17 11:39:43 +0300

    50%

--- homework_10/app.js
+++ homework_10/app.js
@@ -22,6 +22,74 @@ class NumberInput extends Input {
 
 let numberInput = new NumberInput("Type numbers...");
 
+// OPTION 1: InputDecorator
+
+const InputDecorator = function(i) {
+    this.ins = i;
+}
+
+InputDecorator.prototype = new Input();
+InputDecorator.prototype = {
+    AddRequiredValidation: function() {
+        this.ins.valid = !((this.ins.value() === "") || (this.ins.value() == null));
+    },
+    AddMaxLengthValidation: function(maxLength) {
+        this.ins.valid = !(this.ins.value() > maxLength);
+    },
+    AddNumberValidation: function() {
+        this.ins.valid = typeof this.ins.value() != "number";
+    }
+}
+
+
+// OPTION 2: InputDecorator2
+
+const InputDecorator2 = function(i) {
+    this.ins = i;
+}
+
+InputDecorator2.prototype = new Input();
+InputDecorator2.prototype = {
+    getValue: function() {
+        this.ins.value();
+    },
+    setValue: function(val) {
+        this.ins.setValue(val);
+    }
+}
+
+let AddRequiredValidation = function(ins) {
+    InputDecorator2.call(this, ins);
+};
+
+AddRequiredValidation.prototype = new InputDecorator2();
+AddRequiredValidation.prototype.setValue = function(val) {
+    this.ins.valid = !((val === "") || (val == null));
+    this.ins.setValue(val);
+    if (!this.ins.valid) console.log("AddRequiredValidation didnt pass");
+}
+
+let AddMaxLengthValidation =  function(maxLength) {
+    InputDecorator2.call(this, ins); 
+};  
+AddMaxLengthValidation.prototype = new InputDecorator2();
+AddMaxLengthValidation.prototype.setValue = function(val) {
+    this.ins.valid = !(val > maxLength);
+    this.ins.setValue(val);
+    if (!this.ins.valid) console.log("AddMaxLengthValidation didnt pass");
+}
+
+let AddNumberValidation = function() {
+    InputDecorator2.call(this, ins);
+};
+AddNumberValidation.prototype = new InputDecorator2();
+AddNumberValidation.prototype.setValue = function(val) {
+    this.ins.valid = typeof val != "number";
+    this.ins.setValue(val);
+    if (!this.ins.valid) console.log("AddMaxLengthValidation didnt pass");
+}
+
+
 //  Then you can create add validation decorators and add functionality to numberInput
 //  AddRequiredValidation Decorator that add required validation
 //  AddMaxLengthValidation Decorator that add max length validation
