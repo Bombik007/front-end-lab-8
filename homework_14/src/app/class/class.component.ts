1e209914d7780f9679b9b87efb8586abfa0e5d0b
Bohdan Babyak <bodiababiak@gmail.com>
2018-05-15 20:50:00 +0300

    minor visual fixes

--- homework_14/src/app/class/class.component.ts
+++ homework_14/src/app/class/class.component.ts
@@ -1,34 +1,38 @@
-import { Component, Input } from '@angular/core';
-import { Class, Classes } from '../classes';
-import { ScheduleComponent } from '../schedule/schedule.component';
+import {Component, Input} from '@angular/core';
+import {Class, Classes} from '../classes';
+import {ScheduleComponent} from '../schedule/schedule.component';
 
-
-@Component({
-  selector: 'app-class',
-  templateUrl: './class.component.html',
-  styleUrls: ['./class.component.css']
-})
+@Component({selector: 'app-class', templateUrl: './class.component.html', styleUrls: ['./class.component.css']})
 export class ClassComponent {
 
-  @Input() class: Class;
+  @Input()class : Class;
   edit = false;
   public prev;
 
-  public editClass(): void {
+  public editClass() : void {
     this.edit = true;
-    this.prev = {...this.class};
+    this.prev = {
+      ...this.class
+    };
   }
 
-  public discardChanges(): void {
+  public discardChanges() : void {
     this.edit = false;
-    this.class = {...this.prev};
+    this.class = {
+      ...this.prev
+    };
   }
-  public saveClass(): void {
+
+  public saveClass() : void {
     this.edit = false;
+    localStorage.setItem('database', JSON.stringify(Classes));
+  }
+
+  public deleteClass() : void {
+    const index: number = Classes.indexOf(this.class);
+    if (index >= 0) {
+      Classes.splice(index, 1);
       localStorage.setItem('database', JSON.stringify(Classes));
-  }
-
-  public deleteClass(): void {
-    this.class = null;
+    }
   }
 }
